1f3b5d3ab127484ee4b7bccbf488a0ab14a0bbfb
candie <adekanmi.adewumi@straitpay.com>
2024-08-22 10:07:30 +0100

    update profile and delete account integration

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
+const Contact = require("../models/contact");
 const User = require("../models/user");
+const AppError = require("../utils/app-error");
 const catchAsync = require("../utils/catch-async");
 
 exports.viewProfile = catchAsync(async (req, res, next) => {
@@ -6,8 +8,36 @@ exports.viewProfile = catchAsync(async (req, res, next) => {
     "name email createdAt"
   );
 
+  if (!profile) return next(new AppError("User not found", 404));
+
   return res.status(200).json({
     status: "Success",
     data: profile,
+  });
+});
+
+exports.updateProfile = catchAsync(async (req, res, next) => {
+  const { name } = req.body;
+  const profile = await User.findByIdAndUpdate(
+    req.user.id,
+    { name },
+    { new: true, runValidators: true }
+  ).select("name email createdAt");
+  if (!profile) return next(new AppError("User not found", 404));
+
+  return res.status(200).json({
+    status: "Success",
+    data: profile,
+  });
+});
+
+exports.deleteAccount = catchAsync(async (req, res, next) => {
+  const profile = await User.findByIdAndDelete(req.user.id);
+  await Contact.deleteMany({ userId: req.user.id });
+  if (!profile) return next(new AppError("User not found", 404));
+
+  return res.status(204).json({
+    status: "Success",
+    data: null,
   });
 });
--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { viewProfile } = require("../controllers/user");
+const {
+  viewProfile,
+  updateProfile,
+  deleteAccount,
+} = require("../controllers/user");
 
 const { protect } = require("../middleware");
 
 const router = express.Router();
 
 router.get("/", protect, viewProfile);
+router.patch("/", protect, updateProfile);
+router.delete("/", protect, deleteAccount);
 
 module.exports = router;
